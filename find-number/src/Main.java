8987f14a0ac4ba32bd5f0fdbd8b1476c5f50a659
akgunmustafa <makgun095@gmail.com>
2022-12-23 01:23:15 +0300

    sayi-bulmaca

---
+++ find-number/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        int sayac,sayi;
+        int min=1;
+        int max=1;
+
+        System.out.print("Kaç tane sayıyı kontrol etmek istersiniz? : ");
+        sayac= scanner.nextInt();
+
+        for (int i=1;i<=sayac;i++){
+            System.out.print(i+". Sayınızı Giriniz : ");
+            sayi= scanner.nextInt();
+            if (sayi>=max){
+                max=sayi;
+            } else if (sayi<=min) {
+                min=sayi;
+            }
+        }
+        System.out.print("En küçük sayınız : "+min);
+        System.out.print(" ");
+        System.out.print("En büyük sayınız : "+max);
+    }
+}
