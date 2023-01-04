8b74a045f0eda4544ee389215bf0d0d9a78eb059
akgunmustafa <makgun095@gmail.com>
2023-01-04 23:48:39 +0300

    simple-return-methods

---
+++ returnMethods/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    static int sum(int a, int b){
+        int result=a+b;
+        System.out.println(result);
+        return result;
+    }
+    public static void main(String[] args) {
+        int test=sum(5,7);
+    }
+}
