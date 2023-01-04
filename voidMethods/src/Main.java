4d22e482fb161c516f25a288a8b83e1ba14928b4
akgunmustafa <makgun095@gmail.com>
2023-01-04 23:51:44 +0300

    simple-void-methods

---
+++ voidMethods/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    static void sum(int a, int b){
+        int result=a+b;
+        System.out.println(result);
+    }
+    public static void main(String[] args) {
+        sum(5,9);
+        
+    }
+}
